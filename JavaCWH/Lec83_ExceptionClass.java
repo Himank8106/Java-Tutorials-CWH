bb5589403449e4701e96d7e8e6f0224cc4952d0d
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-06-22 23:51:25 +0530

    Update Lec83_ExceptionClass.java

--- JavaCWH/Lec83_ExceptionClass.java
+++ JavaCWH/Lec83_ExceptionClass.java
@@ -25,6 +25,8 @@ public class Lec83_ExceptionClass {
                 System.out.println(e.getMessage()); 
                 System.out.println(e.toString()); 
                 e.printStackTrace();
+                /* This method prints the stack trace of the exception, including the line numbers and method calls that 
+                   led to the exception. It helps in debugging the code by showing where the exception occurred.  */
                 System.out.println("Hii");
             }
         }
